a36fae23cc29cfa9fb723e8924413b2f4ee8fa02
Steffen Barr <smb3402@rit.edu>
2022-11-29 20:52:22 -0500

    created campaign selections and candidate info pages, js files for each, and renamed ballot.css to styles.css

---
+++ assets/js/can_info.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function xhr(getPost, url, data) {
+  return $.ajax({
+    type: getPost,
+    data: data,
+    dataType: "json",
+    cache: false,
+    async: true,
+    url: url,
+  }).fail(function (err) {
+    console.log(err);
+  });
+}
+
---
+++ assets/js/selection.js
@@ -0,0 +1,41 @@
+"use strict";
+
+function xhr(getPost, url, data) {
+  return $.ajax({
+    type: getPost,
+    data: data,
+    dataType: "json",
+    cache: false,
+    async: true,
+    url: url,
+  }).fail(function (err) {
+    console.log(err);
+  });
+}
+
+$(document).ready(function () {
+    const query = window.location.search;
+    const params = new URLSearchParams(query);
+    const societyId = params.get("society_id");
+
+    xhr("get", `http://localhost:3000/api/societies/${societyId}`, {}).done(
+        function (json) {
+            const society = json[0];
+
+            const title = `<h2>${society.name}</h2>`;
+            const list = `<ul id="selections"></ul`;
+            $("#title-div").append(title);
+            $("#campaign-div").append(list);
+        }
+    )
+  
+    xhr("get", `http://localhost:3000/api/societies/campaigns/${societyId}`, {}).done(
+      function (json) {
+        const campaign = json;
+        campaign.forEach((element) => {
+            let name = `<li><a href="./ballot.html?campaign_id=${element.campaign_id}">${element.name}</a></li>`;
+            $("#selections").append(name);
+        });
+      }
+    );
+});
